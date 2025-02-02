4104c804ee3b8a59c909948c16941da6583769cb
Igor Katson <igor.katson@gmail.com>
2025-02-02 12:39:59 +0000

    Update RTTE further according to rfc6298

--- src/rtte.rs
+++ src/rtte.rs
@@ -1,29 +1,38 @@
 use std::time::{Duration, Instant};
 
-use tracing::trace;
-
 use crate::seq_nr::SeqNr;
 
-// NOTE: RTT = round trip time
-// NOTE: RTO = retransmission time out
+// rfc6298: 2.1. Until a round-trip time (RTT) measurement has been made for a
+// segment sent between the sender and receiver, the sender SHOULD
+// set RTO <- 1 second
+const RTTE_INITIAL_RTT: Duration = Duration::from_millis(300);
 
-// Conservative initial RTT estimate.
-const RTTE_INITIAL_RTT: u32 = 300;
-const RTTE_INITIAL_DEV: u32 = 100;
+// rfc6298 2.4
+const RTTE_MIN_RTO: Duration = Duration::from_secs(1);
+// rfc6298 2.4
+const RTTE_MAX_RTO: Duration = Duration::from_secs(60);
 
-// Minimum "safety margin" for the RTO that kicks in when the
-// variance gets very low.
-const RTTE_MIN_MARGIN: u32 = 5;
+fn clamp(rto: Duration) -> Duration {
+    rto.clamp(RTTE_MIN_RTO, RTTE_MAX_RTO)
+}
 
-const RTTE_MIN_RTO: u32 = 10;
-const RTTE_MAX_RTO: u32 = 10000;
+const K: u32 = 4;
+
+#[derive(Debug, Clone, Copy)]
+enum RttState {
+    Initial {
+        rto: Duration,
+    },
+    Subsequent {
+        rto: Duration,
+        srtt: Duration,
+        rttvar: Duration,
+    },
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct RttEstimator {
-    // Using u32 instead of Duration to save space (Duration is i64)
-    rtt: u32,
-    deviation: u32,
-    rto_count: u8,
+    state: RttState,
 
     #[cfg(test)]
     forced_timeout: Option<Duration>,
@@ -32,9 +41,9 @@ pub struct RttEstimator {
 impl Default for RttEstimator {
     fn default() -> Self {
         Self {
-            rtt: RTTE_INITIAL_RTT,
-            deviation: RTTE_INITIAL_DEV,
-            rto_count: 0,
+            state: RttState::Initial {
+                rto: RTTE_INITIAL_RTT,
+            },
 
             #[cfg(test)]
             forced_timeout: None,
@@ -54,28 +63,32 @@ impl RttEstimator {
             return t;
         }
 
-        let margin = RTTE_MIN_MARGIN.max(self.deviation * 4);
-        let ms = (self.rtt + margin).clamp(RTTE_MIN_RTO, RTTE_MAX_RTO);
-        Duration::from_millis(ms as u64)
+        match &self.state {
+            RttState::Initial { rto } => *rto,
+            RttState::Subsequent { rto, .. } => *rto,
+        }
     }
 
     fn sample(&mut self, new_rtt: Duration) {
-        // "Congestion Avoidance and Control", Van Jacobson, Michael J. Karels, 1988
-        self.rtt = (self.rtt * 7 + new_rtt.as_millis() as u32 + 7) / 8;
-        let diff = (self.rtt as i32 - new_rtt.as_millis() as i32).unsigned_abs();
-        self.deviation = (self.deviation * 3 + diff + 3) / 4;
+        match &mut self.state {
+            RttState::Initial { .. } => {
+                let srtt = new_rtt;
+                let rttvar = new_rtt / 2;
 
-        // This was there in smoltcp, however it seems to interfere - retransmission
-        self.rto_count = 0;
+                let rto = clamp(srtt + rttvar * K); // SRTT + max (G, K*RTTVAR);
+                self.state = RttState::Subsequent { rto, srtt, rttvar }
+            }
+            RttState::Subsequent { rto, srtt, rttvar } => {
+                // RTTVAR <- (1 - beta) * RTTVAR + beta * |SRTT - R'| = rttvar * 3 / 4 + (srtt - new_rtt).abs() / 4
+                // SRTT <- (1 - alpha) * SRTT + alpha * R' = srtt * 7 / 8 + new_rtt / 8 = (srtt * 7 + new_rtt) / 8
+                // alpha = 1/8
+                // beta = 1/4
 
-        let rto = self.retransmission_timeout().as_millis();
-        trace!(
-            "rtte: sample={:?} rtt={:?} dev={:?} rto={:?}",
-            new_rtt,
-            self.rtt,
-            self.deviation,
-            rto
-        );
+                *rttvar = *rttvar * 3 / 4 + srtt.abs_diff(new_rtt) / 4;
+                *srtt = (*srtt * 7 + new_rtt) / 8;
+                *rto = clamp(*srtt + K * *rttvar);
+            }
+        }
     }
 
     pub fn on_send(&mut self, _timestamp: Instant, _seq: SeqNr) {
@@ -87,23 +100,14 @@ impl RttEstimator {
     }
 
     pub fn on_retransmit(&mut self) {
-        self.rto_count = self.rto_count.saturating_add(1);
-        if self.rto_count >= 3 {
-            // This happens in 2 scenarios:
-            // - The RTT is higher than the initial estimate
-            // - The network conditions change, suddenly making the RTT much higher
-            // In these cases, the estimator can get stuck, because it can't sample because
-            // all packets sent would incur a retransmit. To avoid this, force an estimate
-            // increase if we see 3 consecutive retransmissions without any successful sample.
-            self.rto_count = 0;
-            self.rtt = RTTE_MAX_RTO.min(self.rtt * 2);
-            let rto = self.retransmission_timeout().as_millis();
-            trace!(
-                "rtte: too many retransmissions, increasing: rtt={:?} dev={:?} rto={:?}",
-                self.rtt,
-                self.deviation,
-                rto
-            );
-        }
+        // rfc6298 section 5
+        match &mut self.state {
+            RttState::Initial { rto } => {
+                *rto = clamp(*rto * 2);
+            }
+            RttState::Subsequent { rto, .. } => {
+                *rto = clamp(*rto * 2);
+            }
+        };
     }
 }
