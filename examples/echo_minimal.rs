a4c7b2c5bc83927049f8bbaadc16e1b37819d65a
Igor Katson <igor.katson@gmail.com>
2025-02-17 13:59:21 +0000

    Preparing for 0.2.0 release

--- examples/echo_minimal.rs
+++ examples/echo_minimal.rs
@@ -4,7 +4,7 @@ pub use librqbit_utp::UtpSocket;
 
 use librqbit_utp::UtpStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tracing::info;
+use tracing::{error_span, info, Instrument};
 
 const MAX_COUNTER: u64 = 1_000_000;
 const PRINT_EVERY: u64 = 100_000;
@@ -43,17 +43,23 @@ async fn main() {
     let client_addr: SocketAddr = (Ipv4Addr::LOCALHOST, 8001).into();
     let server_addr: SocketAddr = (Ipv4Addr::LOCALHOST, 8002).into();
 
-    let client = tokio::spawn(async move {
-        let client = UtpSocket::new_udp(client_addr).await.unwrap();
-        let sock = client.connect(server_addr).await.unwrap();
-        echo(sock).await
-    });
+    let client = tokio::spawn(
+        async move {
+            let client = UtpSocket::new_udp(client_addr).await.unwrap();
+            let sock = client.connect(server_addr).await.unwrap();
+            echo(sock).await
+        }
+        .instrument(error_span!("client")),
+    );
 
-    let server = tokio::spawn(async move {
-        let listener = UtpSocket::new_udp(server_addr).await.unwrap();
-        let sock = listener.accept().await.unwrap();
-        echo(sock).await
-    });
+    let server = tokio::spawn(
+        async move {
+            let listener = UtpSocket::new_udp(server_addr).await.unwrap();
+            let sock = listener.accept().await.unwrap();
+            echo(sock).await
+        }
+        .instrument(error_span!("server")),
+    );
 
     let (cr, sr) = tokio::join!(client, server);
     cr.unwrap();
